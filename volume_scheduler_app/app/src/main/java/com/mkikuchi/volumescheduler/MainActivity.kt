350a4bda9d8475d134eb75f7fbb1a04db9f201d5
m-kikuchi777 <tnct.km.777@gmail.com>
2019-07-24 20:48:31 +0900

    プロジェクトを新規作成

---
+++ volume_scheduler_app/app/src/main/java/com/mkikuchi/volumescheduler/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mkikuchi.volumescheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
